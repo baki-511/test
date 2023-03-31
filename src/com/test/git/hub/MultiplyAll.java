c095faac9660d146d7a508c086acef7447fd61c3
baki-511 <johanbaki381@gmail.com>
2023-03-31 18:00:00 +0530

    Add multiply features to all branch

---
+++ src/com/test/git/hub/MultiplyAll.java
@@ -0,0 +1,12 @@
+package com.test.git.hub;
+
+public class MultiplyAll {
+    public int multiply(int c, int d) {
+        return c * d;
+    }
+
+    public static void main(String[] args) {
+        MultiplyAll mul = new MultiplyAll();
+        System.out.println("Multiplication of Two number = "+mul.multiply(34,23));
+    }
+}
